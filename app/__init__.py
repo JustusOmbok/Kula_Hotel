7b6ad6f1124e70ad2932f710cbebbed207aa5dee
JustusOmbok <ombokjustus@gmail.com>
2023-12-12 06:54:04 +0000

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+CORS(app)
 app.secret_key = '34293560'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hotel.db'
 
